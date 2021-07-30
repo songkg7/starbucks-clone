13c1ab7be917146eea4d3bb1c617e562d90db74f
Haril Song <songkg7@gmail.com>
2021-07-30 12:25:46 +0900

    feat: badge - header

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+// NOTE: _.throttle(func, time)
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // hidden badge
+      // NOTE: gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
