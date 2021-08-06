009b351d3774507bd4a3bd710355f6c767448417
Haril Song <songkg7@gmail.com>
2021-08-06 15:41:38 +0900

    style: visual

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ toTopEl.addEventListener("click", function () {
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
-    delay: (index + 1) * 0.7,
+    delay: (index + 1) * 0.5,
     opacity: 1,
   });
 });
